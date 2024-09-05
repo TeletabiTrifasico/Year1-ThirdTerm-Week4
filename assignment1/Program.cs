dc2b77bd9cc7b64b5be36bc88ca96437a8306189
Hugo Jiménez Barrasa <47745777+TeletabiTrifasico@users.noreply.github.com>
2024-09-05 17:42:57 +0200

    Add files via upload

---
+++ assignment1/Program.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using Model;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+
+        void Start()
+        {
+            Customer customer = new Customer(0, "lionelmessi@hotmail.com", "Lionel", "Messi");
+            Customer customer1 = new Customer(0, "donhenley@gmail.com", "Don", "Henley");
+
+            Book book = new Book(0, "Harry Potter and the Prisoner of Azkaban", "J.K. Rowling");
+            Book book1 = new Book(0, "The Da Vince Code", "Dan Brown");
+
+            Reservation reservation = new Reservation(0, customer, book1);
+            Reservation reservation1 = new Reservation(0, customer1, book1);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("testing customers");
+            Console.ResetColor();
+            Console.WriteLine(customer.ToString());
+            Console.WriteLine(customer1.ToString());
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("testing books");
+            Console.ResetColor();
+            Console.WriteLine(book.ToString());
+            Console.WriteLine(book1.ToString());
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("testing reservations");
+            Console.ResetColor();
+            Console.WriteLine(reservation.ToString());
+            Console.WriteLine(reservation1.ToString());
+        }
+    }
+}
---
+++ assignment2/Program.cs
@@ -0,0 +1,52 @@
+﻿using DAL;
+using Model;
+
+namespace assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+        }
+        void Start()
+        {
+            CustomerDAO customerDAO = new CustomerDAO();
+            List<Customer> customers = customerDAO.GetAllCustomers();
+
+            foreach(Customer customer in customers)
+            {
+                Console.WriteLine(customer.ToString());
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter customer id: ");
+            int customerId = int.Parse(Console.ReadLine());
+            Customer customer1 = new Customer();
+            customer1 = customerDAO.GetById(customerId);
+            if(customer1 != null)
+                Console.WriteLine(customer1.ToString());
+            else
+                Console.WriteLine($"No customer with id: {customerId}");
+
+            BookDAO bookDAO = new BookDAO();   
+            List<Book> books = bookDAO.GetAllBooks();
+
+            foreach(Book book in books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter book id: ");
+            int bookId = int.Parse(Console.ReadLine());
+            Book book1 = new Book();
+            book1 = bookDAO.GetById(bookId);
+            if (book1 != null)
+                Console.WriteLine(book1.ToString());
+            else
+                Console.WriteLine($"No customer with id: {bookId}");
+        }
+    }
+}
